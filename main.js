3abb7c1f9ef6f609649679a92ce9cbf8577c5996
Soso1010 <99210584+Soso1010@users.noreply.github.com>
2023-03-12 15:11:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -30,30 +30,31 @@ function draw(){
         g = random(255);
         b = random(255);
         objectDetector.detect(video,gotResult);
-        for (i = 0; i < objects.lenght; i++){
-           document.getElementById("status").innerHTML = " Status: Objetos detectados";
+        for (i = 0; i < objects.lenght; i++)
+        {
+            document.getElementById("status").innerHTML = " Status: Objetos detectados";
             fill(r,g,b);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
             noFill();
             stroke(r,g,b);
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-        }
-if(objects[i].label == "person")
-{
-    document.getElementById("numberOfObjects").innerHTML = "Bebêencontrado";
-    console.log("stop");
-    song.stop();
-}
-        else
-        {
-            document.getElementById("numberOfObjects").innerHTML = "Bebê não  encontrado";
-            console.log("play");
-            song.play();
+           if(objects[i].label == "person")
+            {
+                document.getElementById("numberOfObjects").innerHTML = "Bebê encontrado";
+                console.log("stop");
+                song.stop();
+            }
+            else
+            {
+                document.getElementById("numberOfObjects").innerHTML = "Bebê não  encontrado";
+                console.log("play");
+                song.play();
+            }
+        } 
         }
         if(objects.lenght == 0)
         {
-document.getElementById("numberOfObjects").innerHTML = "BebÊ não encontrado"
+document.getElementById("numberOfObjects").innerHTML = "Bebê não encontrado"
             console.log("play");
             song.play();
     }
@@ -67,7 +68,7 @@ function modelLoaded(){
 function gotResult(error, results){
     if (error) {
         console.log(error)
-        objects = results;
+        
     }
     console.log(results);
     objects = results;
